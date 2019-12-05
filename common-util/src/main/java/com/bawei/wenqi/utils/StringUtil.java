c0b4231be04c399b386d50dcb53ff151ed1f457d
414768963 <414768963@qq.com>
2019-12-05 18:45:45 +0800

    第三次修改字符串工具类

--- common-util/src/main/java/com/bawei/wenqi/utils/StringUtil.java
+++ common-util/src/main/java/com/bawei/wenqi/utils/StringUtil.java
@@ -1,5 +1,8 @@
 package com.bawei.wenqi.utils;
 
+import java.util.Iterator;
+import java.util.Random;
+
 /** 
  * @Title: StringUtil.java 
  * @Package com.bawei.wenqi.utils 
@@ -10,6 +13,7 @@ package com.bawei.wenqi.utils;
  */ 
 public class StringUtil {
 
+	private static Random random = new Random();
 	/**  
 	* @Title: isBlank  
 	* @Description: TODO(判断字符串是否为空)  
@@ -64,8 +68,69 @@ public class StringUtil {
 		return str.matches(regex);
 		
 	}
+
+	/**  
+	* @Title: isEmail  
+	* @Description: TODO(判断是否全为数字s)  
+	* @param @param str
+	* @param @return    设定文件  
+	* @return boolean    返回类型  
+	*/
+	public static boolean isAllLetters(String str) {
+		String regex="[a-zA-Z]{1,}";
+		return str.matches(regex);
+		
+	}
+	
+	/**  
+	* @Title: getRandomAzChar  
+	* @Description: 获取一个随机字母
+	* @param @return    设定文件  
+	* @return String    返回类型  
+	*/
+	public static char getRandomAzChar() {
+		int newChar = 'a' +random.nextInt(26);
+		return (char)newChar;
+	}
+	
+	/**  
+	* @Title: getRandomNumChar  
+	* @Description: 获取一个0-9随机数
+	* @param @return    设定文件  
+	* @return char    返回类型  
+	*/
+	public static char getRandomNumChar() {
+		int newChar = '0'+random.nextInt(10);
+		return (char)newChar;
+	}
+	
+	//传入的参数为产生的随机英文字母个数
+	public static String getLetters(int num) {
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < num; i++) {
+			sb.append(getRandomAzChar());
+		} 
+		
+		return sb.toString();
+	}
+	
+	//传入的参数为产生的随机英文和数字字符串长度
+	public static String getLettersOrNum(int num) {
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < num; i++) {
+			if(random.nextInt(36)>10) {
+				sb.append(getRandomAzChar());
+			}else {
+				sb.append(getRandomNumChar());
+			}
+		} 
+		
+		return sb.toString();
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(isEmail("414768963@qq.com"));
+		System.out.println(getLettersOrNum(10));
 	}
 }
