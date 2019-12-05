8b5fd6f3b2971a94a10e9acbd42744eb6e864fca
414768963 <414768963@qq.com>
2019-12-05 14:19:29 +0800

    第一次修改字符串工具类

--- common-util/src/main/java/com/bawei/wenqi/utils/StringUtil.java
+++ common-util/src/main/java/com/bawei/wenqi/utils/StringUtil.java
@@ -1,6 +1,5 @@
 package com.bawei.wenqi.utils;
 
-
 /** 
  * @Title: StringUtil.java 
  * @Package com.bawei.wenqi.utils 
@@ -11,4 +10,49 @@ package com.bawei.wenqi.utils;
  */ 
 public class StringUtil {
 
+	/**  
+	* @Title: isBlank  
+	* @Description: TODO(判断字符串是否为空)  
+	* @param @param str
+	* @param @return    设定文件  
+	* @return boolean    返回类型  
+	*/
+	public static boolean isBlank(String str) {
+		
+		//为空返回true
+		if(str==null || str.equals("") || str.trim().length()==0) {
+			return true;
+		}
+		
+		//不为空返回false
+		return false;
+	}
+	
+	/**  
+	* @Title: isNotBlank  
+	* @Description: TODO(判断是否有值)  
+	* @param @param str
+	* @param @return    设定文件  
+	* @return boolean    返回类型  
+	*/
+	public static boolean isNotBlank(String str) {
+		return !isBlank(str);
+	}
+	
+	/**  
+	* @Title: isPhoneNum  
+	* @Description: TODO(判断是否是手机号)  
+	* @param @param str
+	* @param @return    设定文件  
+	* @return boolean    返回类型  
+	*/
+	public static boolean isPhoneNum(String str) {
+		String regex="^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\\\d{8}$";
+		return str.matches(regex);
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(isBlank(""));
+	}
 }
