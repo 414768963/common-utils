285e3e2e9a4580f446372e94d986f3429a064e75
414768963 <414768963@qq.com>
2019-12-05 14:29:39 +0800

    第二次修改字符串工具类

--- common-util/src/main/java/com/bawei/wenqi/utils/StringUtil.java
+++ common-util/src/main/java/com/bawei/wenqi/utils/StringUtil.java
@@ -47,12 +47,25 @@ public class StringUtil {
 	* @return boolean    返回类型  
 	*/
 	public static boolean isPhoneNum(String str) {
-		String regex="^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\\\d{8}$";
+		String regex="^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+		return str.matches(regex);
+		
+	}
+	
+	/**  
+	* @Title: isEmail  
+	* @Description: TODO(判断是否为邮箱账号)  
+	* @param @param str
+	* @param @return    设定文件  
+	* @return boolean    返回类型  
+	*/
+	public static boolean isEmail(String str) {
+		String regex="^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
 		return str.matches(regex);
 		
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(isBlank(""));
+		System.out.println(isEmail("414768963@qq.com"));
 	}
 }
